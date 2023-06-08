88a9053907b43be185f6f3b7b98971f4e4e9d6bc
Oleg Kuvshinov <oleg@o-kuv.ru>
2023-06-09 02:29:44 +0300

    tag model

--- blog/views.py
+++ blog/views.py
@@ -5,12 +5,13 @@ from .models import Post
 from .forms import PostForm
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(active=True).order_by('published_date')
+    posts = Post.objects.filter(active=True).order_by('created_date')
     return render (request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render (request, 'blog/post_detail.html', {'post': post})
+    tags = post.tags.filter(active=True)
+    return render (request, 'blog/post_detail.html', {'post': post, 'tags': tags})
 
 @login_required
 def post_new (request):
@@ -19,7 +20,6 @@ def post_new (request):
         if form.is_valid():
             post = form.save(commit = False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -34,7 +34,6 @@ def post_edit (request, pk):
         if form.is_valid():
             post = form.save(commit = False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
