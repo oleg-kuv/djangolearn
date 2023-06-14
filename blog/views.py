1a04644b8008fc338f711705b5993a99e9c2306a
Oleg Kuvshinov <oleg@o-kuv.ru>
2023-06-15 00:35:07 +0300

    add filtering by get query params

--- blog/views.py
+++ blog/views.py
@@ -3,38 +3,53 @@ from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 # Create your views here.
+
+
 def post_list(request):
-    posts = Post.objects.filter(active=True).order_by('created_date')
-    return render (request, 'blog/post_list.html', {'posts': posts})
+    filter_active = request.GET.get('active')
+    filter_tag = request.GET.get('tag')
+
+    if filter_active is None:
+        filter_active = True
+
+    posts = Post.objects.order_by('created_date').filter(active=filter_active)
+
+    if filter_tag is not None:
+        posts = posts.filter(tags=filter_tag)
+
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     tags = post.tags.filter(active=True)
-    return render (request, 'blog/post_detail.html', {'post': post, 'tags': tags})
+    return render(request, 'blog/post_detail.html', {'post': post, 'tags': tags})
+
 
 @login_required
-def post_new (request):
+def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit = False)
+            post = form.save(commit=False)
             post.author = request.user
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render (request, 'blog/post_edit.html', {'form': form })
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 @login_required
-def post_edit (request, pk):
+def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post = form.save(commit = False)
+            post = form.save(commit=False)
             post.author = request.user
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render (request, 'blog/post_edit.html', {'form': form })
+    return render(request, 'blog/post_edit.html', {'form': form})
