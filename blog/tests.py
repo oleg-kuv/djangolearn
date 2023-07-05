c103b429a7cf478c2188d5166c6def1969bade38
Oleg Kuvshinov <oleg@o-kuv.ru>
2023-07-05 15:18:41 +0300

    fix test

--- blog/tests.py
+++ blog/tests.py
@@ -3,33 +3,54 @@ from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 from blog.models import Post
 
-
-class TestBlog(TestCase):
-    def setUp(self):
-        self.add_new_page = '/post/add_new/'
-        self.edit_page = '/post/1/edit/'
-        self.group_author = Group.objects.create(name='test_author')
-        post_ct_qs = ContentType.objects.get_for_model(Post)
-        post_permissions = Permission.objects.filter(content_type=post_ct_qs)
-
-        for permission in post_permissions:
-            if permission.codename == 'add_post' or permission.codename == 'change_post':
-                self.group_author.permissions.add(permission)
-
-        self.test_pass = "1qazxsw2"
-        self.author_user = User.objects.create_user(
-            "testAuthorUser", "testauthoruser@example.com", self.test_pass)
-        self.author_user.groups.add(self.group_author)
-        self.simple_user = User.objects.create_user(
-            "testUser", "testuser@example.com",  self.test_pass)
-
-    def tearDown(self):
-        self.author_user.delete()
-        self.simple_user.delete()
-        self.group_author.delete()
+test_pass = 'a;sldkfj123'
 
 
-class LoginTestCase(TestBlog):
+def create_author_group_helper():
+    group_author = Group.objects.create(name='test_author')
+    post_ct_qs = ContentType.objects.get_for_model(Post)
+    post_permissions = Permission.objects.filter(content_type=post_ct_qs)
+
+    for permission in post_permissions:
+        if permission.codename == 'add_post' or permission.codename == 'change_post':
+            group_author.permissions.add(permission)
+    return group_author
+
+
+def create_author_user_helper(username="testAuthorUser"):
+    group_author = create_author_group_helper()
+    user = User.objects.create_user(
+        username, f"{username}@example.com", test_pass)
+    user.groups.add(group_author)
+    return user
+
+
+def create_reader_user_helper():
+    user = User.objects.create_user(
+        "testUser", "testuser@example.com", test_pass)
+    return user
+
+
+def remove_test_user_helper(user: User):
+    user.delete()
+
+
+def create_post_helper(author_user_object: User, title="Test post", active=True):
+    post = Post()
+    post.title = title
+    post.text = 'text'
+    post.active = active
+    post.author = author_user_object
+    post.save()
+
+    return post
+
+
+def remove_new_post_helper(post: Post):
+    post.delete()
+
+
+class LoginTestCase(TestCase):
     # Вход с несуществующим логином
     def test_non_exists_login(self):
         login = self.client.login(
@@ -38,60 +59,75 @@ class LoginTestCase(TestBlog):
 
     # Вход с существующим логином
     def test_login(self):
-        login = self.client.login(
-            username=self.simple_user.username,
-            password=self.test_pass
-        )
-        self.assertTrue(login)
+        user = create_reader_user_helper()
 
-    # Вход автора
-    def test_author_login(self):
         login = self.client.login(
-            username=self.author_user.username,
-            password=self.test_pass
+            username=user.username,
+            password=test_pass
         )
         self.assertTrue(login)
 
 
-class AccessTestCase(TestBlog):
-
+class AccessTestCase(TestCase):
     def test_index(self):
         # Доступность главной
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_unauthorised(self):
+    def test_unauthorised_create_post(self):
         # Отсутствие доступа неавторизованного к добавлению поста
-        response = self.client.get(self.add_new_page)
+        response = self.client.get('/post/add_new/')
         self.assertEqual(response.status_code, 302)
+        self.assertEqual('/accounts/login/?next=/post/add_new/', response.url)
 
+    def test_unauthorised_edit_post(self):
         # Отсутствие доступа неавторизованного к редактированию поста
-        response = self.client.get(self.edit_page)
-        self.assertEqual(response.status_code, 302)
+        author = create_author_user_helper()
+        new_post = create_post_helper(author)
 
-    def test_authorised_NO_author(self):
+        response = self.client.get(f"/post/{new_post.id}/edit/")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            f"/accounts/login/?next=/post/{new_post.id}/edit/", response.url)
+
+    def test_authorised_simple_user_create_post(self):
         # Отсутствие доступа простого авторизованного пользователя к созданию поста
-        self.client.login(username=self.simple_user.username,
-                          password=self.test_pass)
-        response = self.client.get(self.add_new_page)
-        self.assertEqual(response.status_code, 302)
+        user = create_reader_user_helper()
 
+        self.client.login(username=user.username,
+                          password=test_pass)
+        response = self.client.get('/post/add_new/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual('/accounts/login/?next=/post/add_new/', response.url)
+
+    def test_authorised_simple_user_edit_post(self):
         # Отсутствие доступа простого авторизованного пользователя к редактированию поста
-        self.client.login(username=self.simple_user.username,
-                          password=self.test_pass)
-        response = self.client.get(self.edit_page)
-        self.assertEqual(response.status_code, 302)
+        user = create_reader_user_helper()
+        author = create_author_user_helper()
+        new_post = create_post_helper(author)
 
-    def test_authorised_author(self):
+        self.client.login(username=user.username,
+                          password=test_pass)
+        response = self.client.get(f"/post/{new_post.id}/edit/")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            f"/accounts/login/?next=/post/{new_post.id}/edit/", response.url)
+
+    def test_authorised_author_create_post(self):
         # Наличие доступа автора к созданию поста
-        self.client.login(username=self.author_user.username,
-                          password=self.test_pass)
-        response = self.client.get(self.add_new_page)
+        author = create_author_user_helper()
+
+        self.client.login(username=author.username,
+                          password=test_pass)
+        response = self.client.get('/post/add_new/')
         self.assertEqual(response.status_code, 200)
 
-        # Наличие доступа к редактированию поста
-        # По какой то причине не может получить доступ к странице '/post/1/edit/' - 404
-#        login = self.client.login(username=self.author_user.username,
-#                                  password=self.test_pass)
-#        response = self.client.get(self.edit_page)
-#        self.assertEqual(response.status_code, 200)
+    def test_authorised_author_edit_post(self):
+        # Наличие доступа редактированию к созданию поста
+        author = create_author_user_helper()
+        new_post = create_post_helper(author)
+
+        self.client.login(username=author.username,
+                          password=test_pass)
+        response = self.client.get(f"/post/{new_post.id}/edit/")
+        self.assertEqual(response.status_code, 200)
